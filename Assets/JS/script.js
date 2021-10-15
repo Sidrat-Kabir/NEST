af68ab0275408c68f658166c9fe4525428479f1f
Sidrat-Kabir <86687007+Sidrat-Kabir@users.noreply.github.com>
2021-10-15 19:00:58 +0100

    Update script.js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -10,11 +10,17 @@ $(function(){
 
     $('.banner_wrapper').slick({
         fade: true,
-        speed: 700,
+        speed: 1000,
         autoplay: true,
         autoplaySpeed: 2000,
-        prevArrow: "<i class='fas fa-chevron-left banner_icon'></i>",
-        nextArrow: "<i class='fas fa-chevron-right banner_icon'></i>",
+        prevArrow: "<i class='fas fa-chevron-left banner_icon slider_icon'></i>",
+        nextArrow: "<i class='fas fa-chevron-right banner_icon slider_icon'></i>",
         dots: true,     
+    })
+
+    $('.feature_slider').slick({
+        slidesToShow: 10,
+        prevArrow: "<i class='fas fa-arrow-left slider_icon feature_icon'></i>",
+        nextArrow: "<i class='fas fa-arrow-right slider_icon feature_icon'></i>",
     })
 })
